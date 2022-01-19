d77b3787374e581920be1d91c59edf5fe636a002
Jonathan <Jonathan.Diez@gmx.de>
2022-01-19 12:41:24 +0200

    mit umgebauten vocabeln

--- lib/Project2/Search/vocab.dart
+++ lib/Project2/Search/vocab.dart
@@ -1,29 +1,103 @@
 class Vocab {
   final int id;
-  final String englishMain;
+  final String type;
   final String arabicMain;
-  final String exampleSentence;
+  final String englishMain;
+  final String englishMaincom;
+  final String english2;
+  final String english2com;
+  final String english3;
+  final String english3com;
+  final String english4;
+  final String english4com;
+  final String verb;
+  final String verbEng;
+  final String nomVerbPastAra;
+  final String nomVerbPastEng;
+  final String nomVerbPresAra;
+  final String nomVerbPresEng;
+  final String noun;
+  final String nounEng;
+  final String adjective;
+  final String adjectiveEng;
+  final String masder;
+  final String ex1ENG;
+  final String ex1ARA;
+  final String ex2ENG;
+  final String ex2ARA;
+  final String ex3ENG;
+  final String ex3ARA;
+  final String vERBprepositions;
+  final String vERBform;
+  final String nOUNtype;
+  final String nOUNplural;
+  final String nounpluralType;
+  final String aDJECTIVEfemale;
+  final String aDJECTIVEplMale;
+  final String aDJECTIVEplFemale;
+  final String aDJECTIVEpltype;
+  final String pREPex1ENG;
+  final String pREPex1ARA;
+  final String pREPex2ENG;
+  final String pREPex2ARA;
+  final String pREPex3ENG;
+  final String pREPex3ARA;
+  final String pREPex4ENG;
+  final String pREPex4ARA;
+
 
   const Vocab({
     required this.id,
+    required this.type,
     required this.arabicMain,
     required this.englishMain,
-    required this.exampleSentence,
+    required this.englishMaincom,
+    required this.english2,
+    required this.english2com,
+    required this.english3,
+    required this.english3com,
+    required this.english4,
+    required this.english4com,
+    required this.verb,
+    required this.verbEng,
+    required this.nomVerbPastAra,
+    required this.nomVerbPastEng,
+    required this.nomVerbPresAra,
+    required this.nomVerbPresEng,
+    required this.noun,
+    required this.nounEng,
+    required this.adjective,
+    required this.adjectiveEng,
+    required this.masder,
+    required this.ex1ENG,
+    required this.ex1ARA,
+    required this.ex2ENG,
+    required this.ex2ARA,
+    required this.ex3ENG,
+    required this.ex3ARA,
+    required this.vERBprepositions,
+    required this.vERBform,
+    required this.nOUNtype,
+    required this.nOUNplural,
+    required this.nounpluralType,
+    required this.aDJECTIVEfemale,
+    required this.aDJECTIVEplMale,
+    required this.aDJECTIVEplFemale,
+    required this.aDJECTIVEpltype,
+    required this.pREPex1ENG,
+    required this.pREPex1ARA,
+    required this.pREPex2ENG,
+    required this.pREPex2ARA,
+    required this.pREPex3ENG,
+    required this.pREPex3ARA,
+    required this.pREPex4ENG,
+    required this.pREPex4ARA,
   });
 
-  factory Vocab.fromJson(Map<String, dynamic> json) => Vocab(
-    id: json['id'],
-    arabicMain: json['author'],
-    englishMain: json['title'],
-    exampleSentence: json['urlImage'],
-  );
-
-  Map<String, dynamic> toJson() => {
+  /*Map<String, dynamic> toJson() => {
     'id': id,
     'title': englishMain,
     'author': arabicMain,
     'urlImage': exampleSentence,
-  };
+  };*/
 }
-
-final List<int> hello = [1,2,3,4,];
